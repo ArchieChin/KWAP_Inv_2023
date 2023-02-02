e1bf0bff96b8fee9c81ff23e785e203d824d5484
ArchieChin <88961370+ArchieChin@users.noreply.github.com>
2023-02-02 22:31:42 +0800

    Create main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ Created on Sun Apr 10 17:12:22 2022
 import streamlit as st
 
 st.title("KWAP 2023 ESG Game")
-st.header("Achieving long-term sustainable investment outcomes")
+st.subheader("Achieving long-term sustainable investment outcomes")
