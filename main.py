e5bf2df39db52a0356e3786f144318f02318afdb
ArchieChin <88961370+ArchieChin@users.noreply.github.com>
2023-02-08 21:24:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import streamlit as st
 from deta import Deta
 
+def insert_entry(player, question, effort, carbon):
+    return db.insert({
+        "player": player,
+        "question": question,
+        "effort": effort,
+        "carbon": carbon
+    })
+    
+
 st.title("KWAP 2023 ESG Game")
 st.subheader("Achieving long-term sustainable investment outcomes")
 
 if st.button("Click"):
-    deta = Deta("project key")
-    users = deta.Base("users")
+    deta = Deta("c0ky03c3_FeGypxDjVhTDCQU96cfUqLkstZLvo6Bb")
+    db = deta.Base("database")
 
-    users.insert({
-        "name": "Geordi",
-        "title": "Chief Engineer"
-    })
-
-    fetch_res = users.fetch({"name": "Geordi"})
+    fetch_res = fb.fetch()
 
     for item in fetch_res.items:
-        users.delete(item["key"])
+        print(item)
+        db.delete(item["key"])
